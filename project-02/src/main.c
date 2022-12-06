14873b6cee25909358f3aad8205a67dc07f6c9c9
xnerad04 <xnerad04@vutbr.cz>
2022-12-06 14:30:57 +0100

    start

---
+++ project-02/src/main.c
@@ -0,0 +1,35 @@
+/***********************************************************************
+ * 
+ * The I2C (TWI) bus scanner tests all addresses and detects chips and
+ * modules that are connected to the SDA and SCL signals.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2017 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+
+/* Defines -----------------------------------------------------------*/
+#ifndef F_CPU
+# define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
+#endif
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <stdlib.h>         // C library. Needed for number conversions
+
+#define PWM PB0
+
+int main(void)
+{
+  int rucka = GPIO_READ(&PINB, PWM);
+  rucka = rucka + 5;
+  
+}
